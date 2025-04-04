79c185613583eab1e867e605623d13bd51038342
sarah-williams1 <sarah.t.williams@outlook.com>
2025-04-04 13:56:31 -0400

    Identified purpose of app
    Added purpose block
    Added 'Assets' folder
    Added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+DNA Sequence Analyzer
+Created by: Sarah Williams
+
+Purpose: 
+    1. Count nucleotides.
+    2. Identify GC content (DNA stability).
+    3. Identify proteins.
+    4. Identify 5' and 3' ends.
+    5. Data export.
+'''
+
